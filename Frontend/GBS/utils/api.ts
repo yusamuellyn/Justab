8f34b1c8f94a9360ba3731248470b6018d663695
Samuel <syu12@binghamton.edu>
2026-06-12 00:01:00 -0400

    My changes

--- Frontend/GBS/utils/api.ts
+++ Frontend/GBS/utils/api.ts
@@ -2,13 +2,9 @@ import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
 export function getApiUrl() {
-  return 'https://disclose-charger-tainted.ngrok-free.dev';
+  const host =
+    Constants.expoGoConfig?.debuggerHost?.split(':')[0] ??
+    Constants.expoConfig?.hostUri?.split(':')[0];
+  if (host) return `http://${host}:8000`;
+  return Platform.OS === 'android' ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
 }
-
-// function getApiUrl() {
-//   const host =
-//     Constants.expoGoConfig?.debuggerHost?.split(':')[0] ??
-//     Constants.expoConfig?.hostUri?.split(':')[0];
-//   if (host) return `http://${host}:8000`;
-//   return Platform.OS === 'android' ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
-// }
